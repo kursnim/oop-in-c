905a0fe14a62ad5a2b3c0437115fac4bcf2e4eb0
Kwansoo Kim <xiriuz@naver.com>
2020-12-21 00:09:39 +0900

    test

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
+#include <stdio.h>
 #include "calc.h"
+#include "shape.h"
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    
+    Shape s1, s2;
+
+    Shape_set(&s1, 0, 1);
+    Shape_set(&s2, -1, 2);
+
+    printf("s1 (%d, %d)\n", Shape_getX(&s1));
+    printf("s2 (%d, %d)\n", Shape_getX(&s2));
+
+    Shape_moveBy(&s1, 2, -4);
+    Shape_moveBy(&s2, 2, -4);
+
+    printf("s1 (%d, %d)\n", Shape_getX(&s1));
+    printf("s2 (%d, %d)\n", Shape_getX(&s2));
+
+    printf("hello~\n");
+
     return 0;
 }
