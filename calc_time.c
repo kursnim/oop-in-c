70663c35fd50107a3e6039c3fc33f0110c1e5f09
Kwansoo Kim <xiriuz@naver.com>
2020-12-24 00:12:31 +0900

    updated 10 arg

--- calc_time.c
+++ calc_time.c
@@ -18,9 +18,10 @@ static long get_nanos(void)
 // int call_argument_6(int a, int b, int c, int d, int e, int f) { return a + b + c + d + e + f; };
 // int call_argument_7(int a, int b, int c, int d, int e, int f, int g) { return a + b + c + d + e + f + g; };
 // int call_argument_8(int a, int b, int c, int d, int e, int f, int g, int h) { return a + b + c + d + e + f + g + h; };
-// int call_argument_9(int a, int b, int c, int d, int e, int f, int g, int h, int i) { return a + b + c + d + e + f + g + h + i; };
-// int call_argument_10(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) { return a + b + c + d + e + f + g + h + i + j; };
-void call_argument_0() { return; }                                                                       //{ return; };
+// int call_argument_9(int a, int b, int c, int d, int e, int f, int g, int h, int 0) { return a + b + c + d + e + f + g + h + i; };
+// int call_argument_10(int a, int b, int c, int d, int e, int f, int g, int h, int 0, int j) { return a + b + c + d + e + f + g + h + i + j; };
+void call_argument_void(void) { return; }                                                                //{ return; };
+int call_argument_0(void) { return 0; }                                                                  //{ return; };
 int call_argument_1(int a) { return 0; }                                                                 //{ return a; };
 int call_argument_2(int a, int b) { return 0; }                                                          //{ return a + b; };
 int call_argument_3(int a, int b, int c) { return 0; }                                                   //{ return a + b + c; };
@@ -32,79 +33,85 @@ int call_argument_8(int a, int b, int c, int d, int e, int f, int g, int h) { re
 int call_argument_9(int a, int b, int c, int d, int e, int f, int g, int h, int i) { return 0; }         //{ return a + b + c + d + e + f + g + h + i; };
 int call_argument_10(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) { return 0; } //{ return a + b + c + d + e + f + g + h + i + j; };
 
-#define LOOP_SIZE (100000000)
+#define LOOP_SIZE (300000000)
 int main(void)
 {
     long time_ns[100];
-    long i;
+    long j;
 
     time_ns[0] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
         call_argument_0();
     }
     time_ns[1] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_1(i);
+        call_argument_1(0);
     }
     time_ns[2] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_2(i, i);
+        call_argument_2(0, 0);
     }
     time_ns[3] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_3(i, i, i);
+        call_argument_3(0, 0, 0);
     }
     time_ns[4] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_4(i, i, i, i);
+        call_argument_4(0, 0, 0, 0);
     }
     time_ns[5] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_5(i, i, i, i, i);
+        call_argument_5(0, 0, 0, 0, 0);
     }
     time_ns[6] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_6(i, i, i, i, i, i);
+        call_argument_6(0, 0, 0, 0, 0, 0);
     }
     time_ns[7] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_7(i, i, i, i, i, i, i);
+        call_argument_7(0, 0, 0, 0, 0, 0, 0);
     }
     time_ns[8] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_8(i, i, i, i, i, i, i, i);
+        call_argument_8(0, 0, 0, 0, 0, 0, 0, 0);
     }
     time_ns[9] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_9(i, i, i, i, i, i, i, i, i);
+        call_argument_9(0, 0, 0, 0, 0, 0, 0, 0, 0);
     }
     time_ns[10] = get_nanos();
 
-    for (i = 0; i < LOOP_SIZE; i++)
+    for (j = 0; j < LOOP_SIZE; j++)
     {
-        call_argument_10(i, i, i, i, i, i, i, i, i, i);
+        call_argument_10(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     }
     time_ns[11] = get_nanos();
+
+    for (j = 0; j < LOOP_SIZE; j++)
+    {
+        call_argument_void();
+    }
+    time_ns[12] = get_nanos();
 
     printf("%ld\n", time_ns[1] - time_ns[0]);
     printf("%ld\n", time_ns[2] - time_ns[1]);
@@ -117,6 +124,7 @@ int main(void)
     printf("%ld\n", time_ns[9] - time_ns[8]);
     printf("%ld\n", time_ns[10] - time_ns[9]);
     printf("%ld\n", time_ns[11] - time_ns[10]);
+    printf("%ld\n", time_ns[12] - time_ns[11]);
 
     return EXIT_SUCCESS;
 }
