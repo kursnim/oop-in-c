5081f0adfdf50f4a6487142545554f9c51b614aa
kursnim <kursnim@gmail.com>
2020-12-24 22:40:11 +0900

    commit test

--- main.c
+++ main.c
@@ -1,22 +1,48 @@
-#include <stdio.h>
 #include "calc.h"
 #include "shape.h"
+#include <stdio.h>
+
+typedef struct
+{
+    struct struct_data
+    {
+        int x;
+        int y;
+    } data;
+
+    int (*fp)(int, int);
+} tShape;
+
+int Shape_sum(int a, int b)
+{
+    return a + b;
+}
 
 int main(int argc, char const *argv[])
 {
-    Shape s1, s2;
 
-    Shape_set(&s1, 0, 1);
-    Shape_set(&s2, -1, 2);
+    tShape Shape;
+    Shape.data.x = 1;
+    Shape.data.y = 2;
+    Shape.fp = Shape_sum;
 
-    printf("s1 (%d, %d)\n", Shape_getX(&s1));
-    printf("s2 (%d, %d)\n", Shape_getX(&s2));
+    printf("%d %d\n", Shape.data.x, Shape.data.y);
+    printf("%d\n", Shape.fp(1, 2));
 
-    Shape_moveBy(&s1, 2, -4);
-    Shape_moveBy(&s2, 2, -4);
+    // tClassShape s1, s2;
+    // tShape a;
 
-    printf("s1 (%d, %d)\n", Shape_getX(&s1));
-    printf("s2 (%d, %d)\n", Shape_getX(&s2));
+    // Shape_set(&s1, 0, 1);
+    // Shape_set(&s2, -1, 2);
+
+    // printf("s1 (%d, %d)\n", Shape_getX(&s1));
+    // printf("s2 (%d, %d)\n", Shape_getX(&s2));
+
+    // Shape_moveBy(&s1, 2, -4);
+    // Shape_moveBy(&s2, 2, -4);
+
+    // printf("s1 (%d, %d)\n", Shape_getX(&s1));
+    // printf("s2 (%d, %d)\n", Shape_getX(&s2));
 
     printf("hello~\n");
 
